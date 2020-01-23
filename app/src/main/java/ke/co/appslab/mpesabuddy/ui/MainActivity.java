4b3cd47b8fc8f7703322d6097171913ebbc72002
Harun Wangereka <harunwangereka@gmail.com>
2020-01-23 19:55:36 +0300

    Added UI

--- app/src/main/java/ke/co/appslab/mpesabuddy/ui/MainActivity.java
+++ app/src/main/java/ke/co/appslab/mpesabuddy/ui/MainActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import ke.co.appslab.mpesabuddy.R;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button initiatePaymentBtn;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    initiatePaymentBtn = findViewById(R.id.initiatePaymentButton);
+
+    initiatePaymentBtn.setOnClickListener(view ->{
+        Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+            );
   }
 }
